bd1bff4a248115106556b6612bc716f7ade3756c
Chelt0n <qys1@yandex.ru>
2021-08-09 14:45:12 +0300

    С помощью костылей добавил темы для всех активити интент-фильтр крашит у меня весь манифист зафтра доделаю

--- app/src/main/java/com/example/calculate/MainMenu.java
+++ app/src/main/java/com/example/calculate/MainMenu.java
@@ -1,9 +1,10 @@
 package com.example.calculate;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(getRealStyle(getCurrentTheme()));
         setContentView(R.layout.activity_menu);
         calculate = findViewById(R.id.calculate);
         settings = findViewById(R.id.settings);
@@ -35,6 +37,23 @@ public class MainMenu extends AppCompatActivity {
         });
 
 
+    }
+
+    private int getCurrentTheme() {
+        SharedPreferences sharedPreferences = getSharedPreferences(Settings.KEY_SP, MODE_PRIVATE);
+        return (sharedPreferences.getInt(Settings.KEY_CURRENT_THEME, -1));
+    }
+
+    private int getRealStyle(int currentTheme) {
+        switch (currentTheme) {
+            case Settings.whiteTheme:
+                return R.style.whiteTheme;
+            case Settings.blackTheme:
+                return R.style.blackTheme;
+
+            default:
+                return 0;
+        }
     }
 
 }
